201cfbad1cbc67322abec34ef2a2c8d9b09415fa
Adityagiri Goswami <aadigithub61@gmail.com>
2026-06-20 14:21:47 +0530

    Add Structure contract with Student management
    Implemented a contract to manage student data with set and get functions.

--- structure.sol
+++ structure.sol
@@ -1,10 +1,37 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-struct Student{
-    
-}
+contract Structure {
 
-contract Structuree{
+    struct Student {
+        string name;
+        uint age;
+        uint rollNo;
+    }
 
+    Student public student;
+
+    function setStudent(
+        string memory _name,
+        uint _age,
+        uint _rollNo
+    ) public {
+        student = Student(_name, _age, _rollNo);
+    }
+
+    function getStudent()
+        public
+        view
+        returns (
+            string memory,
+            uint,
+            uint
+        )
+    {
+        return (
+            student.name,
+            student.age,
+            student.rollNo
+        );
+    }
 }
